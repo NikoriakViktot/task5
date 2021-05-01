6078fa653077a3da14e2c534a711b3aaca782041
NikoriakViktot <83222767+NikoriakViktot@users.noreply.github.com>
2021-05-01 11:38:43 +0300

    Add files via upload

--- Task5.py
+++ Task5.py
@@ -53,3 +53,8 @@ print(unique_number1 + unique_number2)
 # Extracting numbers.
 # Make a list that contains all integers from 1 to 100, then find all integers from the list that are divisible by 7 but not a multiple of 5, and store them in a separate list. Finally, print the list.
 # Constraint: use only while loop for iteration/
+
+
+d_list = list(range(1, 101))
+x = [i for i in d_list if i % 7 == 0 and i % 5]
+print(x)
